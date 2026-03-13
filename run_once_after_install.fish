c8666ba032d62578da1e3e9bf071e330ccc90872
Sandipan Das <sandipan.osd@gmail.com>
2026-03-13 11:05:43 +0530

    chezmoi: Fix scripts
    Reorganize scripts and cleanup dependencies.
    
    Signed-off-by: Sandipan Das <sandipan.osd@gmail.com>

--- run_once_after_install.fish
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env fish
-
-function install__pkgs
-    echo "info: install packages - tmux"
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-    ~/.tmux/plugins/tpm/bin/install_plugins
-end
-
-install__pkgs
--- run_once_before_pre_install.fish
+++ run_once_before_pre_install.fish
@@ -1,36 +1,27 @@
 #!/usr/bin/env fish
 
 function cleanup__pkgs
-    echo "info: cleanup"
-
+    echo "info: cleanup - nvim"
     rm -rf ~/.config/nvim
     rm -rf ~/.local/share/nvim
     rm -rf ~/.local/state/nvim
     rm -rf ~/.cache/nvim
 
+    echo "info: cleanup - fish"
     rm -rf ~/.config/fish
+    mkdir -p ~/.config/fish
+    touch ~/.config/fish/config.fish
+    source ~/.config/fish/config.fish
 
+    echo "info: cleanup - starship"
     rm -rf ~/.config/starship.toml
 
+    echo "info: cleanup - tmux"
     rm -rf ~/.tmux.conf
     rm -rf ~/.tmux
 end
 
 function install__pkgs
-    echo "info: install packages - mise"
-    if not type -q mise
-        curl --silent --show-error --location https://mise.run | sh
-        ~/.local/bin/mise activate fish | source
-    end
-
-    mise use -g fzf@latest
-    mise use -g neovim@latest
-    mise use -g node@latest
-    mise use -g python@latest
-    mise use -g starship@latest
-    mise use -g tmux@latest
-    mise use -g zoxide@latest
-
     echo "info: install packages - fisher"
     curl --silent --show-error --location https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source
     fisher install jorgebucaran/fisher
